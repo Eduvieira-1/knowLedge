e026310353024a6ccf8cabcf14fc2f93486ae030
Eduvieira-1 <edu.vieira1@icloud.com>
2023-06-26 17:41:05 -0300

    Projeto inicio

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		<h1>Versão Inicial</h1>
+		<p>Test</p>
+		<p>Test</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "App",
+}
+</script>
+
+<style>
+
+</style>
