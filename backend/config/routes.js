790b6915254eb2c95dfa89b3ed6a121671da9795
Eduvieira-1 <edu.vieira1@icloud.com>
2023-06-28 17:43:54 -0300

    api artigo #2

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -10,6 +10,11 @@ module.exports = app => {
     app.route('/categories')
         .get(app.api.category.get)
         .post(app.api.category.save)
+
+    //Cuidado com a ordem! tem que vir antes de /categories/:id
+
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
     
     app.route('/categories/:id')
         .get(app.api.category.getById)
