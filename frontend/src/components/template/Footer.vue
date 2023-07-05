4871aa503ec02d8d5b693a7af0346048ca9f59c0
Eduvieira-1 <edu.vieira1@icloud.com>
2023-07-05 11:00:58 -0300

    template start

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong> Cod3r</strong></span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer{
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: end;
+        padding-right: 10px;
+    }
+</style>
