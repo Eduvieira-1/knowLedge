3544d4aff702dd649baf5b02285a303686b87ba6
Eduvieira-1 <edu.vieira1@icloud.com>
2023-07-06 17:56:10 -0300

    config users

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,17 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
+//TEMPORARIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkVkdWFyZG8gQ2VzYXIgU2lsdmVyaW8gVmllaXJhIiwiZW1haWwiOiJlZHVhcmRvQGhvdG1haWwuY29tLmJyIiwiYWRtaW4iOnRydWUsImlhdCI6MTY4ODY3MzI1OSwiZXhwIjoxNjg4OTMyNDU5fQ.9NKBFa-wmk3flK0qKCNPIeQeXY1xxPh4vsobMmBoNHc'
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
