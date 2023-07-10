1bbc5aa814b34329a721e8b10fe408aab3fd9c5b
Eduvieira-1 <edu.vieira1@icloud.com>
2023-07-10 14:43:12 -0300

    registerArticle

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -37,7 +37,13 @@ export default {
 <style>
 .header {
   grid-area: header;
-  background: linear-gradient(to right, #1e469a, #49a7c1);
+  background: #11998e;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #38ef7d, #11998e);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #38ef7d, #11998e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+
+
 
   display: flex;
   justify-content: center;
