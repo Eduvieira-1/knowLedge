cc28f9f3f865451b02c15fba60e313fcc258f3a5
Eduvieira-1 <edu.vieira1@icloud.com>
2023-07-10 15:05:10 -0300

    articleByCategories

---
+++ frontend/src/components/article/ArticleByCategory.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="articles-by-category">
+        <PageTitle icon="fa fa-folder-o" :main="category.name" sub="Categoria" />
+    </div>
+</template>
+
+<script>
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+import PageTitle from '../template/PageTitle.vue'
+export default {
+    name: 'ArticleByCategory',
+    components: { PageTitle },
+    data(){
+        return {
+            category: {},
+            articles: [],
+            page: 1,
+            loadMore: true
+        }
+    },
+    methods:{
+        getCategory(){
+            const url = `${baseApiUrl}/categories/${this.category.id}`
+            axios(url).then(res => this.category = res.data)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
