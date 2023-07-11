1db138b4d4a1b2ca96429566d920ba1ba8ad4c53
Eduvieira-1 <edu.vieira1@icloud.com>
2023-07-11 15:03:48 -0300

    validationUser

--- frontend/src/components/auth/Auth.vue
+++ frontend/src/components/auth/Auth.vue
@@ -3,6 +3,20 @@
     <div class="auth-modal">
         <img src="@/assets/logo.png" width="200" alt="Logo" />
         <hr>
+        <div class="auth-title">{{ showSignup ? 'Cadastro' : 'Login' }}</div>
+
+        <input type="text" v-if="showSignup" v-model="user.name" placeholder="Nome">
+        <input type="text" v-model="user.email" name="email" placeholder="E-mail">
+        <input type="password" v-model="user.password" name="password" placeholder="Senha">
+        <input type="password" v-if="showSignup" v-model="user.confirmPassword" placeholder="Confirme a senha">
+
+        <button class="btn btn-outline-primary" v-if="showSignup" @click="signup">Registrar</button>
+        <button class="btn btn-outline-primary" v-else @click="signin">Entrar</button>
+
+        <a href @click.prevent="showSignup = !showSignup">
+            <span v-if="showSignup">Já tem cadastro ? Acesso o Login!</span>
+            <span v-else>Não tem cadastro ? Registre-se aqui!</span>
+        </a>
     </div>
   </div>
 </template>
@@ -18,6 +32,26 @@ export default {
             showSignup: false,
             user: {}
         }
+    },
+    methods: {
+        signin(){
+            axios.post(`${baseApiUrl}/signin`, this.user)
+                .then(res => {
+                    this.$store.commit('setUser', res.data)
+                    localStorage.setItem(userKey, JSON.stringify(res.data))
+                    this.$router.push({ path: '/'})
+                })
+                .catch(showError)
+        },
+        signup(){
+            axios.post(`${baseApiUrl}/signup`, this.user)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.user = {}
+                    this.showSignup = false
+                })
+                .catch(showError)
+        }
     }
 }
 </script>
@@ -31,6 +65,7 @@ export default {
     }
 
     .auth-modal{
+        border-radius: 10px;
         background-color: #FFF;
         width: 350px;
         padding: 35px;
@@ -39,5 +74,38 @@ export default {
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+    .auth-title {
+        font-size: 1.2rem;
+        font-weight: 100;
+        margin-top: 10px;
+        margin-bottom: 15px;
+    }
+
+    .auth-modal input {
+        border: 1px solid #BBB;
+        width: 100%;
+        margin-bottom: 15px;
+        padding: 3px 8px;
+        outline: none;
+    }
+
+    .auth-modal button {
+        align-self: flex-end;
+        padding: 5px 15px;
+    }
+
+    .auth-modal a {
+        margin-top: 35px;
+    }
+
+    .auth-modal hr {
+        border: 0;
+        width: 100%;
+        height: 1px;
+        background-image: linear-gradient(to right,
+            rgba(120, 120, 120, 0),
+            rgba(120, 120, 120, 0.75),
+            rgba(120, 120, 120, 0));
     }
 </style>
